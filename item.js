0c4e9e43d40fd615fc1333ddfd1d1e802ac7e93e
Metlegen <7nsmalley@stpatrick.org>
2016-12-12 10:53:49 -0600

    Add files via upload

---
+++ item.js
@@ -0,0 +1,13 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor (name, price, shipping){
+  this.name = name;
+  this.price = price;
+  this.shipping = shipping;
+  }
+}
+
+//Create your three test items below this comment.
+DanyloItem = new Item("Balisong", 275, 6);
+BobItem = new Item("Meme", 2000, .5);
+flemdogItem = new Item("COD Ultimate Edition", 80, 10);
